879f225f527aad216c5a2bccde87812735bbca98
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-07-05 03:17:13 +0100

    Delete tiktok.js

--- plugins/download/tiktok.js
+++
@@ -1,7 +0,0 @@
-global.Api = new (require('./path/to/neoxrApi'))('jntZZE')
-
-global.Api.tiktok('https://vt.tiktok.com/ZSe22y3dA').then(result => {
-   console.log(result)
-}).catch(error => {
-   console.log(error)
-})
